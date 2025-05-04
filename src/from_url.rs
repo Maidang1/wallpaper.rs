1d1b5a3ac9e2fb550ed772a9a510b41d0b5142bb
madinah <madinah@madinahdeMac-mini.local>
2025-05-04 19:29:44 +0800

    fix: fix cached image file name

--- src/from_url.rs
+++ src/from_url.rs
@@ -3,7 +3,7 @@ use std::fs::File;
 
 pub fn download_image(url: &str) -> Result<String> {
     let cache_dir = dirs::cache_dir().ok_or("no cache dir")?;
-    let file_path = cache_dir.join("wallpaper");
+    let file_path = cache_dir.join(format!("wallpaper_{}", url.toString()));
 
     let mut file = File::create(&file_path)?;
     reqwest::blocking::get(url)?.copy_to(&mut file)?;
